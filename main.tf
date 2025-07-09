df5e3755ecd6c9f491dc21a416ef89c036607e37
Maneeshnalanda <maneesh202@gmail.com>
2025-07-09 20:44:11 +0530

    my code123

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "azuread_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+resource " storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azuread_resource_group.example.name
+  location                 = azuread_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
